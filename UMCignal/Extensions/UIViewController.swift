764eeaf730c2414fada4ad54219304b3748f89aa
Rudy-009 <rofxnaos@gmail.com>
2025-03-24 11:49:10 +0900

    init: 프로젝트 생성

---
+++ UMCignal/Extensions/UIViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController.swift
+//  UMCignal
+//
+//  Created by 이승준 on 3/14/25.
+//
+
+import UIKit
+
+extension UIViewController {
+        func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
